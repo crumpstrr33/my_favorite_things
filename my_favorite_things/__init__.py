c2aa7ff33affc40c9aeeba8a077159bcc1974d16
crumpstrr33 <jscott137@pm.me>
2023-06-23 09:22:34 -0500

    Add plot.py for matplotlib functions

--- my_favorite_things/__init__.py
+++ my_favorite_things/__init__.py
@@ -1,7 +1,15 @@
 from importlib.metadata import version
 
+from .ddicts import format_ddict, nested_ddict
+from .plot import bar_count, cumulative_bins, log_bins
 from .save import save
-from .ddicts import nested_ddict, format_ddict
 
-__all__ = ["save", "nested_ddict", "format_ddict"]
+__all__ = [
+    "save",
+    "nested_ddict",
+    "format_ddict",
+    "cumulative_bins",
+    "log_bins",
+    "bar_count",
+]
 __version__ = version("my-favorite-things")
