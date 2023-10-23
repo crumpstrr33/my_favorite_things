ba43a9181ad30fc405fe346d9926adbd52687f31
crumpstrr33 <jscott137@pm.me>
2023-10-23 02:45:44 -0500

    Set line length=88 as per black

--- my_favorite_things/colors.py
+++ my_favorite_things/colors.py
@@ -14,9 +14,9 @@ def fader(color1: str, color2: str, fraction: float):
     Parameters:
     color1 - Starting color in hex, e.g. #000000
     color2 - Ending color in hex, e.g. #ffffff
-    fraction - Float between 0 and 1 representing how far from `color1` the
-        output color should be. At the extremes, if `fraction=0`, then we
-        return `color1` and if `fraction=1`, then we return `color2`.
+    fraction - Float between 0 and 1 representing how far from `color1` the output color
+        should be. At the extremes, if `fraction=0`, then we return `color1` and if
+        `fraction=1`, then we return `color2`.
     """
     color1 = np.array(to_rgb(color1))
     color2 = np.array(to_rgb(color2))
@@ -25,20 +25,19 @@ def fader(color1: str, color2: str, fraction: float):
 
 def multi_fader(colors: Sequence[str], fracs: Sequence[float]):
     """
-    Allows for fading between mutliple, equally-spaced colors. Returns a list
-    of colors whose length is equal to that of `fracs` using the function
-    `fader`.
+    Allows for fading between mutliple, equally-spaced colors. Returns a list of colors
+    whose length is equal to that of `fracs` using the function `fader`.
 
     Parameters:
     colors - A list of colors that are considered equally spaced, e.g. if
-        `colors=["#ffffff", "#ff0000", "000000"]` then the total spectrum will
-        shift from white (at 0.0) to red (at 0.5) to black (at 1.0).
-    fracs - A list of floats from 0 to 1 in ascending order representing at
-        which point one wants to sample a point, e.g. using the above example
-        and `fader=[0.3, 0.5, 0.9]` then this returns:
+        `colors=["#ffffff", "#ff0000", "000000"]` then the total spectrum will shift
+        from white (at 0.0) to red (at 0.5) to black (at 1.0).
+    fracs - A list of floats from 0 to 1 in ascending order representing at which point
+        one wants to sample a point, e.g. using the above example and
+        `fader=[0.3, 0.5, 0.9]` then this returns:
             ['#ff6666', '#ff0000', '#cc0000', '#330000']
-        The second color is the 1st element of `colors` since its corresponding
-        `fracs` value was 0.5.
+        The second color is the 1st element of `colors` since its corresponding `fracs`
+        value was 0.5.
     """
     output_colors = []
     # Get evenly spaced fractions for each color
